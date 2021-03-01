f25845ebdea23b67c3f6ed1b17e96e1c76a5cd3d
janee <jane.slevin@ucdconnect.ie>
2021-03-01 22:29:53 +0000

    Finalising, debugging get_weather, reorganising functions

--- JS_harvester.py
+++ JS_harvester.py
@@ -2,11 +2,11 @@ import time
 import datetime
 import requests
 import traceback
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, String, DateTime, insert
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, String, DateTime
 
 bike_key = "7479d89868fa2d94fed820aa4ae3379d64bca75f"
-contract_name = "Dublin"
 bike_URI = "https://api.jcdecaux.com/vls/v1/stations"
+contract_name = "Dublin"
 
 weather_key = "836e60d545402f71b66015366f7b8997"
 weather_URI = "https://api.openweathermap.org/data/2.5/weather"
@@ -18,16 +18,27 @@ password = "js2021dbbikes"
 
 engine = create_engine(f"mysql+mysqlconnector://{name}:{password}@{host}:3306/{db}", echo=True)
 
+def write_to_file(string, obj):
+    '''Write data to file'''
+    title = string
+    current_time = datetime.datetime.now()
+    timestamp = datetime.datetime.timestamp(current_time)
+    with open("{}{}".format(title, timestamp), "w") as f:
+        f.write(obj)
+
 def get_station(obj):
+    '''Pull station data from JSON object'''
     return {"number": obj["number"],
             "name": obj["name"],
             "address": obj["address"],
             "pos_lng": obj["position"]["lng"],
             "pos_lat": obj["position"]["lat"],
-            "bike_stands": obj["bike_stands"]
+            "bike_stands": obj["bike_stands"],
+            "date_created": datetime.datetime.now().replace(microsecond=0)
             }
 
 def get_availability(obj):
+    '''Pull availability data from JSON object'''
     return {"date_created": datetime.datetime.now().replace(microsecond=0),
             "number": obj["number"],
             "bike_stands": obj["bike_stands"],
@@ -37,7 +48,9 @@ def get_availability(obj):
             }
 
 def get_weather(lon, lat, station_num):
+    '''Request weather data for specific geographic coordinates'''
     def get_each_weather(obj, station_num):
+        '''Pull weather data from JSON object'''
         return {"date_created": datetime.datetime.now().replace(microsecond=0),
                 "station_number": station_num,
                 "pos_lng": obj["coord"]["lon"],
@@ -60,24 +73,21 @@ def get_weather(lon, lat, station_num):
                 "last_update": datetime.datetime.fromtimestamp(int(obj["dt"] / 1e3)),
                 "sys_type": obj["sys"]["type"],
                 "sys_id": obj["sys"]["id"],
-                "sunrise": datetime.datetime.fromtimestamp(int(obj["sunrise"] / 1e3)),
-                "sunset": datetime.datetime.fromtimestamp(int(obj["sunset"] / 1e3)),
+                "sunrise": datetime.datetime.fromtimestamp(int(obj["sys"]["sunrise"] / 1e3)),
+                "sunset": datetime.datetime.fromtimestamp(int(obj["sys"]["sunset"] / 1e3)),
                 "timezone": obj["timezone"],
                 "id": obj["id"],
                 "cod": obj["cod"],
                 }
 
     weather_request = requests.get(weather_URI, params={"APPID": weather_key, "lon": lon, "lat": lat})
+    write_to_file("weather", weather_request.text)
+    # Pull weather data from JSON object returned for each set of coordinates
     weather_data = get_each_weather(weather_request.json(), station_num)
     return weather_data
 
-def write_to_file(obj):
-    now = datetime.datetime.now()
-    now_format = now.strftime("%Y%m%d_%H%M%S")
-    with open("availability_{}".format(now_format), "w") as f:
-        f.write(obj)
-
-def scrape_and_store():
+def create_and_fill_stations():
+    '''Create stations table if does not exist, delete existing data from stations table and write updated station data to stations table using get_station function'''
     meta = MetaData()
     stations = Table(
         "stations", meta,
@@ -86,9 +96,23 @@ def scrape_and_store():
         Column("address", String(128)),
         Column("pos_lat", Float),
         Column("pos_lng", Float),
-        Column("bike_stands", Integer)
+        Column("bike_stands", Integer),
+        Column("date_created", DateTime)
     )
+    meta.create_all(engine)
+    r = requests.get(bike_URI, params={"apiKey": bike_key, "contract": contract_name})
+    station_values = list(map(get_station, r.json()))
+    # Clear stations table
+    stationsDelete = stations.delete()
+    engine.execute(stationsDelete)
+    # Insert data into stations table
+    ins_stations = stations.insert().values(station_values)
+    engine.execute(ins_stations)
+    return station_values
 
+def create_and_fill_availability_and_weather(obj):
+    '''Create availability and weather tables if do not exist, add availability and weather data to tables using get_availability and get_weather functions'''
+    meta = MetaData()
     availability = Table(
         "availability", meta,
         Column("date_created", DateTime),
@@ -130,28 +154,30 @@ def scrape_and_store():
         Column("id", Integer),
         Column("cod", Integer),
     )
-
+    # Create tables
     meta.create_all(engine)
     r = requests.get(bike_URI, params={"apiKey": bike_key, "contract": contract_name})
-    station_values = list(map(get_station, r.json()))
-    ins_stations = stations.insert().values(station_values)
-    engine.execute(ins_stations)
-    write_to_file(r.text)
+    write_to_file("bikes", r.text)
     availability_values = list(map(get_availability, r.json()))
+    # Insert data into availability table
     ins_availability = availability.insert().values(availability_values)
     engine.execute(ins_availability)
+    station_values = obj
     for i in station_values:
         station_num = i["number"]
         longitude = i["pos_lng"]
         latitude = i["pos_lat"]
         weather_values = get_weather(*(str(longitude), str(latitude), station_num))
+        # Insert data into weather table
         ins_weather = weather.insert().values(weather_values)
         engine.execute(ins_weather)
 
 def main():
+    '''Call functions to create and write to tables every 5 minutes'''
     while True:
         try:
-            scrape_and_store()
+            station_values = create_and_fill_stations()
+            create_and_fill_availability_and_weather(station_values)
             time.sleep(5 * 60)
         except:
             print(traceback.format_exc())
