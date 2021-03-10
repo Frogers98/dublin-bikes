2b1d199fc7985d7c788b42233fdf21e54cc63a1f
Finnian Rogers <finnianrogers@gmail.com>
2021-03-10 21:46:47 +0000

    initial selector work

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", station_data = get_stations_json())
 
 @app.route("/tables")
 def get_tables():
--- static/app.js
+++ static/app.js
@@ -30,45 +30,46 @@
     }
 
     // Fill the selector - adapted from https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
- let dropdown = document.getElementById('stationSelector');
-dropdown.length = 0;
 
-let defaultOption = document.createElement('option');
-defaultOption.text = 'Choose a Station';
-
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
-
-
-fetch("/stations")
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.warn('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        let option;
-
-    	// for (let i = 0; i < data.length; i++) {
-        //   option = document.createElement('option');
-      	//   option.text = data[i].name;
-      	//   option.value = data[i].abbreviation;
-      	//   dropdown.add(option);
-    	// }
-          data.forEach(station => {
-            option = document.createElement('option');
-            option.text = station.name;
-            option.value = station.name;
-            dropdown.add(option);
-
-          });
-      });
-    }
-  )
-  .catch(function(err) {
-    console.error('Fetch Error -', err);
-  });
+//  let dropdown = document.getElementById('stationSelector');
+// dropdown.length = 0;
+//
+// let defaultOption = document.createElement('option');
+// defaultOption.text = 'Choose a Station';
+//
+// dropdown.add(defaultOption);
+// dropdown.selectedIndex = 0;
+//
+//
+// fetch("/stations")
+//   .then(
+//     function(response) {
+//       if (response.status !== 200) {
+//         console.warn('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
+//
+//       // Examine the text in the response
+//       response.json().then(function(data) {
+//         let option;
+//
+//     	// for (let i = 0; i < data.length; i++) {
+//         //   option = document.createElement('option');
+//       	//   option.text = data[i].name;
+//       	//   option.value = data[i].abbreviation;
+//       	//   dropdown.add(option);
+//     	// }
+//           data.forEach(station => {
+//             option = document.createElement('option');
+//             option.text = station.name;
+//             option.value = station.name;
+//             dropdown.add(option);
+//
+//           });
+//       });
+//     }
+//   )
+//   .catch(function(err) {
+//     console.error('Fetch Error -', err);
+//   });
