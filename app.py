a70b9eb08dd2194ac54a03de6124a020287050bf
Finnian Rogers <finnianrogers@gmail.com>
2021-03-16 17:32:21 +0000

    deleted unused page and associated route/function

--- app.py
+++ app.py
@@ -27,28 +27,6 @@ def hello():
     print(stations)
     return render_template("index.html", station_data = stations)
 
-@app.route("/tables")
-def get_tables():
-    """Function to just display the first five rows of each database table as a table on the webpage"""
-    URI = "dublin-bikes.ciu0f2oznjig.us-east-1.rds.amazonaws.com"
-    PORT = "3306"
-    DB = "dublin_bikes"
-    USER = "admin"
-    PASSWORD = "DublinBikesProject2201"
-    engine = create_engine('mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB, echo=True))
-
-    station_df = pd.read_sql_table("01_station", engine, chunksize = 50)
-    station_df_html = station_df.head(5).to_html()
-
-    weather_df = pd.read_sql_table("01_weather", engine, chunksize = 50)
-    weather_df_html = weather_df.head(5).to_html()
-
-    availability_df = pd.read_sql_table("01_availability", engine, chunksize = 50)
-    availability_df_html = availability_df.head(5).to_html()
-
-    #station_json = df.to_json(orient="records")
-    return render_template("table.html", tables=[station_df_html, weather_df_html, availability_df_html])
-
 @app.route("/stations")
 def stations_request():
     print("in /stations")
