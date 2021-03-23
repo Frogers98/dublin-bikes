5bb5fc50f75164ffb16b30c31e20e60b234b5989
Adam Ryan <adam.ryan@ucdconnect.ie>
2021-03-23 19:55:44 +0000

    removing folders for code merge

--- AR_FlaskApp/MVT-FlaskExperiment/DB_Flask_App/DB_Flask_App/config.py
+++
@@ -1,2 +0,0 @@
-secret_key='adfWESDFZ343SerdgCzsgdr5467uytrtweqrwGDZ'
-db_uri='mysql://adamryan:adam.ryan1@dbbikes.cmbuuvrlonfv.us-east-1.rds.amazonaws.com:3306/dublin_bikes'
--- AR_FlaskApp/MVT-FlaskExperiment/DB_Flask_App/run.py
+++
@@ -1,4 +0,0 @@
-from DB_Flask_App import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
